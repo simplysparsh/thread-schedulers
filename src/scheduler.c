2af2eeb6127ce84decbd2a95d0e5ae9f9d7cc00d
Sparsh Saxena <simplysparsh.s@gmail.com>
2016-10-01 17:48:42 -0400

    Add function to insert element to linked list
    This funtion adds the new data element to the end of the linked list.

--- src/scheduler.c
+++ src/scheduler.c
@@ -28,24 +28,28 @@ float total_wait_time(int tid);
 #define PBS     2
 #define MLFQ    3
 
+int schedulerType = 0;
+
 typedef struct Node Node_t; 
 
 struct Node {
-	
-	int tid;
-	Node_t* link;
+    
+    int tid;
+    Node_t* link;
 };
 
 
 void init_scheduler( int sched_type ) {
-
-	
+    schedulerType = sched_type;
 }
 
 int schedule_me( float currentTime, int tid, int remainingTime, int tprio ) {
     int globalTime = 0;
     Node_t* ready   = NULL;
-    
+
+    if (schedulerType == FCFS) {
+
+    }
 
     return globalTime;
 }
@@ -53,18 +57,43 @@ int schedule_me( float currentTime, int tid, int remainingTime, int tprio ) {
 int num_preemeptions(int tid){
 /**
 
-	Fill your code here
+    Fill your code here
 */
 
-	return -1;
+    return -1;
 }
 
 float total_wait_time(int tid){
 /**
 
-	Fill your code here
+    Fill your code here
 */
 
   return -0.1;
 }
+
+
+/* helper functions */
+
+// Insert data into linked-list
+void insert_to_list(int tid, Node_t* head_addr) {
+
+    Node_t* new_node_address = NULL;
+    Node_t* current = head_addr;
+
+    new_node_address = malloc(sizeof(Node_t));
+    new_node_address -> tid = tid;
+    new_node_address -> link = NULL;
+
+    if (head_addr == NULL) {
+      head_addr = new_node_address;
+    }
+    else {
+      while(current -> link != NULL) {
+       current = current -> link;
+      }
+      current -> link = new_node_address;
+    }
+}
+
 
