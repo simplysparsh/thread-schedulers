54c8654cbfa88bb5732794feef8bfb3e9bec30cf
Sparsh Saxena <simplysparsh.s@gmail.com>
2016-10-09 00:37:36 -0400

    Add scheduler.h header for all function declaration

--- src/scheduler.c
+++ src/scheduler.c
@@ -1,11 +1,11 @@
-/**
+/****************************************************************************
  * 
  * File             : scheduler.c
  * Description      : This is a stub to implement all your scheduling schemes
  *
  * Author(s)        : Sparsh Saxena | Mahdi Chaker  
  * Last Modified    : October 11, 2016
-*/
+*****************************************************************************/
 
 // Include Files
 #include <stdio.h>
@@ -30,22 +30,7 @@ struct Node {
     
 };
 
-void init_scheduler( int sched_type );
-int schedule_me( float currentTime, int tid, int remainingTime, int tprio );
-int num_preemeptions(int tid);
-float total_wait_time(int tid);
-
-//Declaration for Scheduler Functions
-void schedule_with_FCFS(Node_t* current_thread_node, int remainingTime);
-
-//Declaration for helper methods
-Node_t* insert_to_list(float currentTime, int tid, int remainingTime, int tprio);
-void insert_per_fcfs(Node_t* new_node_address);
-void insert_per_pbs(Node_t* new_node);
-
-Node_t* search_list(int tid);
-Node_t* create_new_thread_node(float currentTime, int tid, int remainingTime, int tprio);
-void delete_first_node(Node_t* node_address);
+#include "scheduler.h"
 
 #define FCFS    0
 #define SRTF    1
---
+++ src/scheduler.h
@@ -0,0 +1,25 @@
+/**
+ * 
+ * File             : scheduler.h
+ * Description      : Declarations for functions in scheduler.c
+ * Author(s)        : Sparsh Saxena | Mahdi Chaker  
+ * Last Modified    : October 11, 2016
+*/
+
+
+void init_scheduler( int sched_type );
+int schedule_me( float currentTime, int tid, int remainingTime, int tprio );
+int num_preemeptions(int tid);
+float total_wait_time(int tid);
+
+//Declaration for Scheduler Functions
+void schedule_with_FCFS(Node_t* current_thread_node, int remainingTime);
+
+//Declaration for helper methods
+Node_t* insert_to_list(float currentTime, int tid, int remainingTime, int tprio);
+void insert_per_fcfs(Node_t* new_node_address);
+void insert_per_pbs(Node_t* new_node);
+
+Node_t* search_list(int tid);
+Node_t* create_new_thread_node(float currentTime, int tid, int remainingTime, int tprio);
+void delete_first_node(Node_t* node_address);
